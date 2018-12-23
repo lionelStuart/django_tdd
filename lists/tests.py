93a2d3913c3ef27bb099ccd19847c9d87ebabe3f
lionelStuart <lisi_stuart@126.com>
2018-12-23 16:18:58 +0800

    lists view return html success

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,22 @@
+from django.http import HttpRequest
 from django.test import TestCase
 
-
 # Create your tests here.
+from django.urls import resolve
+from .views import home_page
 
 
-class SmokeTest(TestCase):
+class HomepageTest(TestCase):
 
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        print("resolve func {}".format(found.func))
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>TO-DO</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home_page(request):
+    html = '<html><title>TO-DO</title></html>'
+    return HttpResponse(html)
+    pass
