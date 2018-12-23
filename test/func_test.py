1f0782eecab98c3723bbbd4efa8f19abcd31da97
lionelStuart <lisi_stuart@126.com>
2018-12-23 15:44:27 +0800

    create project djangoWithTDD

--- test/func_test.py
+++ test/func_test.py
@@ -1,9 +1,22 @@
 from selenium import webdriver
+import unittest
 
 HOST = 'http://localhost:8000'
 
 
-browser = webdriver.Firefox()
-browser.get(HOST)
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+       #self.browser.quit()
+        pass
+
+    def test_can_start_with_a_list_and_retrieve_late(self):
+        self.browser.get(HOST)
+        self.assertIn('To-DO', self.browser.title)
+        self.fail('finish the test')
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
